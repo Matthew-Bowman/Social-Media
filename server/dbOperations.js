af56c5a3af8a7f04bee785e67469ecce6635ba2d
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 16:42:40 +0100

    Created database connection in class constructor

--- server/dbOperations.js
+++ server/dbOperations.js
@@ -3,7 +3,24 @@ const mysql = require("mysql2");
 
 // CLASS: Database Connection
 class Connection {
-  constructor() {console.log("Test")}
+  constructor() {
+    // INITIALISE Class Variables
+    this.user = process.env.DB_USER;
+    this.password = process.env.DB_PASS;
+    this.host = process.env.DB_HOST;
+    this.database = process.env.DB_DATABASE;
+    this.port = process.env.DB_PORT;
+    this.connection;
+
+    // CREATE Connection
+    this.connection = mysql.createConnection({
+      host: this.host,
+      user: this.user,
+      password: this.password,
+      database: this.database,
+      port: this.port,
+    });
+  }
 }
 
 module.exports = Connection;
