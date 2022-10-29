db4a7baeead2c501bd6cb05f41b31acca8427579
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 20:18:12 +0100

    Customised appearance of Post Component

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,12 @@
-function Signup({content, timestamp}) {
-    return (
-      <div>
-        <p>{content}</p>
-        <p>{timestamp}</p>
+function Signup({ content, timestamp }) {
+  return (
+    <div className="card text-start col-12 col-md-8 col-lg-6 col-xl-5">
+      <div className="card-body">
+        <p className="m-0 mb-3">{content}</p>
+        <p className="m-0 blockquote-footer">{timestamp}</p>
       </div>
-    );
-  }
-  
-  export default Signup;
-  
+    </div>
+  );
+}
+
+export default Signup;
