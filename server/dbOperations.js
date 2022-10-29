1728ad1226b2dba269892df71abd57530518c7e1
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 16:13:04 +0100

    Setup dbOperations File

---
+++ server/dbOperations.js
@@ -0,0 +1,10 @@
+// REQUIRE Modules
+const mysql = require("mysql2");
+
+// STORE Database Operations
+class dbOperations {
+  constructor() {}
+}
+
+// EXPORT dbOperations
+export default dbOperations;
