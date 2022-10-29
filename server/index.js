48d4165e3c1148a2e66249bdcfa304c7b48abda0
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 15:59:22 +0100

    Added dotenv

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
+// REQUIRE Modules
+require(`dotenv`).config();
 const express = require(`express`);
 const app = express();
 const cors = require("cors");
 
+// INITIALISE Variables
+
+// SETUP App
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3001, () => console.log(`Server Started on Port: 3001`))
+// START Server
+app.listen(3001, () => console.log(`Server Started on Port: 3001`));
