e6f7100e2065290e8ac5fbc6c69433d7dc5990e9
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 17:38:38 +0100

    Removed database util from index.js

--- server/index.js
+++ server/index.js
@@ -3,16 +3,12 @@ require(`dotenv`).config();
 const express = require(`express`);
 const app = express();
 const cors = require("cors");
-const Connection = require(`./utils/database`);
 
 // GET routes
 const api = require(`./routes/api`);
 
 // INITIALISE Variables
 const PORT = process.env.PORT;
-
-// INSTANTIATE DB Connection
-const connection = new Connection();
 
 // SETUP Middleware
 app.use(cors());
