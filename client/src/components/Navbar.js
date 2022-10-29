b79b96fe0ecef1879843e3f1eb5ae7d4e6c85f94
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 21:24:12 +0100

    Reverted how Navbar form submit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,41 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return <div></div>;
+  return (
+    <div>
+      <nav className="navbar bg-dark navbar-dark py-2">
+        <div className="container-fluid">
+          <div className="col-4">
+            <Link className="navbar-brand mb-0 fw-bold fs-3" to="/">
+              <span>Social</span>
+            </Link>
+          </div>
+          <div className="col-4 d-flex justify-content-center">
+            <form
+              className="d-flex gap-2 mx-auto"
+              role="search"
+              onSubmit="/profile"
+            >
+              <div>
+                <input
+                  id="username-input"
+                  className="form-control px-3"
+                  type="search"
+                  placeholder="Search Users"
+                  aria-label="Search Users"
+                  name="username"
+                />
+              </div>
+              <button className="btn btn-outline-light" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="col-4"></div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
