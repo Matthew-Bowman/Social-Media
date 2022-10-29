dbcfb45b033f0f75c1dbc6cca028e3773a163417
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 20:44:42 +0100

    Changed export and name of functional component

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,6 +1,6 @@
-function Signup({ content, timestamp }) {
+function Post({ content, timestamp }) {
   return (
-    <div className="card text-start col-12 col-md-8 col-lg-6 col-xl-5">
+    <div className="card text-start col-12 col-md-8 col-lg-6 col-xl-5 shadow">
       <div className="card-body">
         <p className="m-0 mb-3">{content}</p>
         <p className="m-0 blockquote-footer">{timestamp}</p>
@@ -9,4 +9,4 @@ function Signup({ content, timestamp }) {
   );
 }
 
-export default Signup;
+export default Post;
