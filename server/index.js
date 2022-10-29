9d5fcb8cc8d2b1b48606745624bd0835de66007d
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 15:56:21 +0100

    Setup App

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
 const express = require(`express`);
 const app = express();
 const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3001, () => console.log(`Server Started on Port: 3001`))
