f4f78ddf58609bcb48f187b102cdaf3080d4d771
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 18:27:34 +0100

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Test</h1>
-      </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/settings" element={<Settings />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   );
 }
