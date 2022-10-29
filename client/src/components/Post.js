706b464d22c8e6daf5bf4c540acb1840c55e9b8a
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-29 19:38:23 +0100

    Created components

---
+++ client/src/components/Post.js
@@ -0,0 +1,12 @@
+function Signup({author, content, timestamp}) {
+    return (
+      <div>
+        <h1>{author}</h1>
+        <p>{content}</p>
+        <p>{timestamp}</p>
+      </div>
+    );
+  }
+  
+  export default Signup;
+  
