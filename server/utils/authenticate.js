be74b14f613c7480142ef6abdf73bbe506e3043f
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-30 17:31:53 +0000

    Created authentication middleware

--- server/utils/authenticate.js
+++ server/utils/authenticate.js
@@ -3,7 +3,37 @@ require(`dotenv`).config();
 const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
+  // CHECK for cookies
+  if (!req.headers.cookie) {
+    res.status(404);
+    res.json({ code: 404, message: "Missing Cookies" });
+  } else {
+    // CHECK for auth_token cookie
+    const cookies = req.headers.cookie.split("; ").map((val) => val.split("="));
+    const cookiesObj = Object.assign(...cookies.map(([k, v]) => ({ [k]: v })));
 
-}
+    if (!cookiesObj.auth_token) {
+      res.status(404);
+      res.json({ code: 404, message: "Missing auth_token Cookie" });
+    } else {
+      // VERIFY jsonwebtoken
+      jwt.verify(
+        cookiesObj.auth_token,
+        process.env.JWT_SECRET,
+        (err, decoded) => {
+          if (err) {
+            res.status(401);
+            res.json({ code: 401, message: "Unauthorized" });
+          } else {
+            if (!decoded) {
+              res.status(401);
+              res.json({ code: 401, message: "Unauthorized" });
+            } else next();
+          }
+        }
+      );
+    }
+  }
+};
 
 module.exports = authenticate;
