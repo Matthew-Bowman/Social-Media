86ad12a8cfe68a6fbff8492c7d74c8efda572c09
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-30 21:41:47 +0000

    Changed way auth token obtained

--- server/utils/authenticate.js
+++ server/utils/authenticate.js
@@ -4,34 +4,29 @@ const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
   // CHECK for cookies
-  if (!req.headers.cookie) {
+  if (!req.cookies) {
     res.status(404);
     res.json({ code: 404, message: "Missing Cookies" });
   } else {
     // CHECK for auth_token cookie
-    const cookies = req.headers.cookie.split("; ").map((val) => val.split("="));
-    const cookiesObj = Object.assign(...cookies.map(([k, v]) => ({ [k]: v })));
-
-    if (!cookiesObj.auth_token) {
+    if (!req.cookies["auth_token"]) {
       res.status(404);
-      res.json({ code: 404, message: "Missing auth_token Cookie" });
+      res.json({ code: 404, message: "Missing Token Cookie" });
     } else {
       // VERIFY jsonwebtoken
-      jwt.verify(
-        cookiesObj.auth_token,
-        process.env.JWT_SECRET,
-        (err, decoded) => {
-          if (err) {
+      const token = req.cookies["auth_token"]
+      console.log(token);
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          res.status(401);
+          res.json({ code: 401, message: "Unauthorized" });
+        } else {
+          if (!decoded) {
             res.status(401);
             res.json({ code: 401, message: "Unauthorized" });
-          } else {
-            if (!decoded) {
-              res.status(401);
-              res.json({ code: 401, message: "Unauthorized" });
-            } else next();
-          }
+          } else next();
         }
-      );
+      });
     }
   }
 };
