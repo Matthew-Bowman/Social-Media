c3e5887bf2a4543b5b3618ceabece4fe0563ef08
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-31 03:24:13 +0000

    Created CretePost Component

---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,12 @@
+function CreatePost() {
+  return (
+    <div className="card text-start col-12 col-md-8 col-lg-6 col-xl-5 shadow text-center p-2">
+      <div className="card-body">
+        <p className="m-0 mb-3">Feel like posting something?</p>
+        <button className="btn btn-outline-primary">Create Post</button>
+      </div>
+    </div>
+  );
+}
+
+export default CreatePost;
