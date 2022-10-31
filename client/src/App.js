93359f5465e15203d24d69a4744681c8cefbd6c4
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-31 02:33:09 +0000

    Added Route Protection

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,20 +7,21 @@ import Signup from "./pages/Signup";
 import Settings from "./pages/Settings";
 import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
+import React from "react";
 
 function App() {
-  // const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
 
   return (
     <BrowserRouter>
       {/* SHOW Navbar */}
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
+        <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/settings" element={<Settings />} />
-        <Route exact path="/profile/:username" element={<Profile />} />
+        <Route exact path="/settings" element={isLoggedIn ? <Settings /> : <Navigate to="/login" />} />
+        <Route exact path="/profile/:username" element={isLoggedIn ? <Profile /> : <Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
