1b8853b534437a7184dafccbd94d119e9d8f1af2
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-31 03:49:34 +0000

    Added react-router-dom Link to create route

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 function CreatePost() {
   return (
     <div className="card text-start col-12 col-md-8 col-lg-6 col-xl-5 shadow text-center p-2">
       <div className="card-body">
         <p className="m-0 mb-3">Feel like posting something?</p>
-        <button className="btn btn-outline-primary">Create Post</button>
+        <Link to="/create">
+          <button className="btn btn-outline-primary">Create Post</button>
+        </Link>
       </div>
     </div>
   );
