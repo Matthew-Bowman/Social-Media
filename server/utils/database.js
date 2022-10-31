a1836b00aaa978c244a7fa5d17228c2d87ee56a8
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-31 14:28:36 +0000

    Added GetUserIDbyPostID operation

--- server/utils/database.js
+++ server/utils/database.js
@@ -122,6 +122,23 @@ class Connection {
         });
       });
     };
+
+    // METHOD: returns a user_id based on the post_id
+    this.GetUserIDbyPostID = (postID) => {
+      // SETUP database query
+      const query = `SELECT * FROM post WHERE post_id=?`;
+      const inserts = [postID];
+      const sql = mysql.format(query, inserts);
+
+      // RETURN results
+      return new Promise((resolve, reject) => {
+        // PERFORM Query
+        this.connection.query(sql, (err, res) => {
+          if(err) reject(err);
+          else resolve(res);
+        })
+      });
+    };
   }
 }
 
