a1436b2d91f39893cd693bd21266055e91dd6d8b
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-31 02:47:18 +0000

    Set axios default withCredentials true

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
 import React from "react";
 
+import axios from "axios";
+axios.defaults.withCredentials = true;
+
 function App() {
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
 
@@ -17,11 +20,22 @@ function App() {
       {/* SHOW Navbar */}
       <Navbar />
       <Routes>
-        <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
+        />
         <Route path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/settings" element={isLoggedIn ? <Settings /> : <Navigate to="/login" />} />
-        <Route exact path="/profile/:username" element={isLoggedIn ? <Profile /> : <Navigate to="/login" />} />
+        <Route
+          exact
+          path="/settings"
+          element={isLoggedIn ? <Settings /> : <Navigate to="/login" />}
+        />
+        <Route
+          exact
+          path="/profile/:username"
+          element={isLoggedIn ? <Profile /> : <Navigate to="/login" />}
+        />
       </Routes>
     </BrowserRouter>
   );
