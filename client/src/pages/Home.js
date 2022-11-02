841c23b37c2122ae2c25f61f986c83f6e203faaa
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-11-02 16:49:22 +0000

    Added Loading Appearance whilst HTTP Requests Made

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,20 +2,22 @@ import CreatePost from "../components/CreatePost";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Post from "../components/Post";
+import PostPlaceholder from "../components/PostPlaceholder";
 
 function Home() {
   // INITIALISE data state
   const [posts, setPosts] = useState([]);
   const [username, setUsername] = useState("");
+  const [postsLoaded, setPostsLoaded] = useState(false);
+  const [usernameLoaded, setUsernameLoaded] = useState(false);
 
   const loadPosts = async () => {
     try {
-      const response = await axios.get(
-        `/api/me/posts`,
-        null,
-        { withCredentials: true }
-      );
+      const response = await axios.get(`/api/me/posts`, null, {
+        withCredentials: true,
+      });
       setPosts(response.data.body.posts);
+      setPostsLoaded(true);
     } catch (err) {
       // HANDLE Error
     }
@@ -27,6 +29,7 @@ function Home() {
         withCredentials: true,
       });
       setUsername(response.data.body.user.username);
+      setUsernameLoaded(true);
     } catch (err) {
       // HANDLE Error
     }
@@ -40,19 +43,32 @@ function Home() {
   return (
     <div className="container-fluid p-0 py-5">
       <div className="row mt-5 mx-5 gy-3 d-flex flex-column align-items-center text-center">
-        <h1>Hello, {username}</h1>
-        <CreatePost />
-        {posts.map((item, index) => {
-          return (
-            <Post
-              originalContent={item.content}
-              timestamp={item.timestamp}
-              id={item.post_id}
-              alterable={true}
-              key={index}
-            />
-          );
-        })}
+        {postsLoaded && usernameLoaded ? (
+          <>
+            <h1>Hello, {username}</h1>
+            <CreatePost />
+            {posts.map((item, index) => {
+              return (
+                <Post
+                  originalContent={item.content}
+                  timestamp={item.timestamp}
+                  id={item.post_id}
+                  alterable={true}
+                  key={index}
+                />
+              );
+            })}
+          </>
+        ) : (
+          <>
+            <h1
+              tabIndex="-1"
+              className="btn btn-secondary disabled placeholder col-2"
+              aria-hidden="true"
+            ></h1>
+            <PostPlaceholder alterable={true} />
+          </>
+        )}
       </div>
     </div>
   );
