2a2f7ec73eb51f30d803d22f31ec6d361fddcec2
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-11-04 15:11:01 +0000

    Moved methods outside of constructor

--- server/utils/database.js
+++ server/utils/database.js
@@ -22,216 +22,215 @@ class Connection {
       database: this.database,
       port: this.port,
     });
-
-    // PUBLIC METHODS
-
-    // METHOD: Get user from user table matching usernames
-    this.GetUserByUsername = (username) => {
-      // SETUP database query
-      const query = `SELECT * FROM user WHERE username=?`;
-      const inserts = [username];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
-
-    // METHOD: Get user from user table matching id
-    this.GetUserByUserID = (id) => {
-      // SETUP database query
-      const query = `SELECT username,user_id FROM user WHERE user_id=?`;
-      const inserts = [id];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
-
-    // METHOD: Get posts related to a user
-    this.GetPostsByUserID = (userID, requsetingUserID) => {
-      // SETUP database query
-      const query = `SELECT * FROM post WHERE user_id=? ORDER BY post_id DESC`;
-      const inserts = [userID];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else {
-            const promises = res.map((post) =>
-              this.IsPostLiked(post.post_id, requsetingUserID).then(
-                (liked) => (post.liked = liked)
-              )
-            );
-            Promise.all(promises).then(() => resolve(res));
-          }
-        });
-      });
-    };
-
-    // METHOD: Adds a user to the database
-    this.CreateUser = (username, password) => {
-      // SETUP database query
-      const query = "INSERT INTO user (username, password) VALUES (?, ?)";
-      const inserts = [
-        username,
-        bcrypt.hashSync(password, bcrypt.genSaltSync(5)),
-      ];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
-
-    // METHOD: Compares two passwords
-    this.AuthorizeUser = (password, hashedPassword) => {
-      return bcrypt.compareSync(password, hashedPassword);
-    };
-
-    // METHOD: Create a post
-    this.CreatePost = (userID, content) => {
-      // INITIALISE variables
-      const date = new Date(Date.now());
-      const formattedDate = `${date.getUTCDate()}/${date.getUTCMonth()}/${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}`;
-
-      // SETUP database query
-      const query =
-        "INSERT INTO post (user_id, content, timestamp) VALUES (?, ?, ?)";
-      const inserts = [userID, content, formattedDate];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
-
-    // METHOD: returns a user_id based on the post_id
-    this.GetUserIDbyPostID = (postID) => {
-      // SETUP database query
-      const query = `SELECT * FROM post WHERE post_id=?`;
-      const inserts = [postID];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
-
-    // METHOD: deletes a post based on the post_id
-    this.DeletePostByPostID = (postID) => {
-      // SETUP database query
-      const query = "DELETE FROM post WHERE post_id=?";
-      const inserts = [postID];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
-
-    // METHOD: updates a post's content based on the post_id
-    this.UpdatePostByPostID = (postID, content) => {
-      // SETUP database query
-      const query = "UPDATE post SET content=? WHERE post_id=?";
-      const inserts = [content, postID];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
-
-    // METHOD: checks if a user has liked a post
-    this.IsPostLiked = (postID, userID) => {
-      // SETUP database query
-      const query = "SELECT * FROM liked_post WHERE post_id=? AND user_id=?";
-      const inserts = [postID, userID];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(!(res.length <= 0));
-        });
-      });
-    };
-
-    // METHOD: adds a liked post
-    this.LikePost = (postID, userID) => {
-      // SETUP database query
-      const query = "INSERT INTO liked_post (post_id, user_id) VALUES (?, ?)";
-      const inserts = [postID, userID];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
-
-    // METHOD: adds a liked post
-    this.UnlikePost = (postID, userID) => {
-      // SETUP database query
-      const query = "DELETE FROM liked_post WHERE post_id=? AND user_id=?";
-      const inserts = [postID, userID];
-      const sql = mysql.format(query, inserts);
-
-      // RETURN results
-      return new Promise((resolve, reject) => {
-        // PERFORM Query
-        this.connection.query(sql, (err, res) => {
-          if (err) reject(err);
-          else resolve(res);
-        });
-      });
-    };
   }
+  // PUBLIC METHODS
+
+  // METHOD: Get user from user table matching usernames
+  GetUserByUsername = (username) => {
+    // SETUP database query
+    const query = `SELECT * FROM user WHERE username=?`;
+    const inserts = [username];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  // METHOD: Get user from user table matching id
+  GetUserByUserID = (id) => {
+    // SETUP database query
+    const query = `SELECT username,user_id FROM user WHERE user_id=?`;
+    const inserts = [id];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  // METHOD: Get posts related to a user
+  GetPostsByUserID = (userID, requsetingUserID) => {
+    // SETUP database query
+    const query = `SELECT * FROM post WHERE user_id=? ORDER BY post_id DESC`;
+    const inserts = [userID];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else {
+          const promises = res.map((post) =>
+            this.IsPostLiked(post.post_id, requsetingUserID).then(
+              (liked) => (post.liked = liked)
+            )
+          );
+          Promise.all(promises).then(() => resolve(res));
+        }
+      });
+    });
+  };
+
+  // METHOD: Adds a user to the database
+  CreateUser = (username, password) => {
+    // SETUP database query
+    const query = "INSERT INTO user (username, password) VALUES (?, ?)";
+    const inserts = [
+      username,
+      bcrypt.hashSync(password, bcrypt.genSaltSync(5)),
+    ];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  // METHOD: Compares two passwords
+  AuthorizeUser = (password, hashedPassword) => {
+    return bcrypt.compareSync(password, hashedPassword);
+  };
+
+  // METHOD: Create a post
+  CreatePost = (userID, content) => {
+    // INITIALISE variables
+    const date = new Date(Date.now());
+    const formattedDate = `${date.getUTCDate()}/${date.getUTCMonth()}/${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}`;
+
+    // SETUP database query
+    const query =
+      "INSERT INTO post (user_id, content, timestamp) VALUES (?, ?, ?)";
+    const inserts = [userID, content, formattedDate];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  // METHOD: returns a user_id based on the post_id
+  GetUserIDbyPostID = (postID) => {
+    // SETUP database query
+    const query = `SELECT * FROM post WHERE post_id=?`;
+    const inserts = [postID];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  // METHOD: deletes a post based on the post_id
+  DeletePostByPostID = (postID) => {
+    // SETUP database query
+    const query = "DELETE FROM post WHERE post_id=?";
+    const inserts = [postID];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  // METHOD: updates a post's content based on the post_id
+  UpdatePostByPostID = (postID, content) => {
+    // SETUP database query
+    const query = "UPDATE post SET content=? WHERE post_id=?";
+    const inserts = [content, postID];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  // METHOD: checks if a user has liked a post
+  IsPostLiked = (postID, userID) => {
+    // SETUP database query
+    const query = "SELECT * FROM liked_post WHERE post_id=? AND user_id=?";
+    const inserts = [postID, userID];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(!(res.length <= 0));
+      });
+    });
+  };
+
+  // METHOD: adds a liked post
+  LikePost = (postID, userID) => {
+    // SETUP database query
+    const query = "INSERT INTO liked_post (post_id, user_id) VALUES (?, ?)";
+    const inserts = [postID, userID];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
+
+  // METHOD: adds a liked post
+  UnlikePost = (postID, userID) => {
+    // SETUP database query
+    const query = "DELETE FROM liked_post WHERE post_id=? AND user_id=?";
+    const inserts = [postID, userID];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  };
 }
 
 module.exports = Connection;
