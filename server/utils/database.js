9a483de712ee50de68488cb80a46e27843b26294
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-11-05 02:14:31 +0000

    Created GetPostByPostID operation

--- server/utils/database.js
+++ server/utils/database.js
@@ -148,6 +148,23 @@ class Connection {
     });
   };
 
+  // METHOD: returns posts based on the post_id
+  GetPostByPostID = (postID) => {
+    // SETUP database query
+    const query = "SELECT FROM post WHERE post_id=?";
+    const inserts = [postID];
+    const sql = mysql.format(query, inserts);
+
+    // RETURN results
+    return new Promise((resolve, reject) => {
+      // PERFORM Query
+      this.connection.query(sql, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  }
+
   // METHOD: deletes a post based on the post_id
   DeletePostByPostID = (postID) => {
     // SETUP database query
